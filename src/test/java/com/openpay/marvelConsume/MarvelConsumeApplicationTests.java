4f2d925bf3e959ae9db107c4a269ddb88b3e86f5
Benjamin Mac <benjamin94833@gmail.com>
2024-03-16 20:37:34 -0600

    commit inicial

---
+++ src/test/java/com/openpay/marvelConsume/MarvelConsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openpay.marvelConsume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelConsumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
